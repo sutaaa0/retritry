54c856ec3d9aec851b1aa38228caa334a714f110
sutaaa0 <sutadev0@gmail.com>
2025-07-17 10:17:10 +0700

    create 6 files

---
+++ prisma/migrations/20250717021903_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Training" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "mode" TEXT NOT NULL,
+    "score" INTEGER NOT NULL,
+    "accuracy" INTEGER NOT NULL,
+    "reactionMs" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Training_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Training" ADD CONSTRAINT "Training_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
